41355b3e454eb0df7c3666f3664b8379fa1a8f02
ooooose <yuuki.oose1990@gmail.com>
2024-01-15 21:20:16 +0900

    Add 記事削除・編集APIを作成

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -18,3 +18,40 @@ export const GET = async (req: Request, res: NextResponse) => {
     await prisma.$disconnect();
   }
 };
+
+// ブログの記事編集API
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+
+    const {title, description} = await req.json();
+
+    await main();
+    const post = await prisma.post.update({
+      data: {title, description},
+      where: { id },
+    });
+    return NextResponse.json({ message: 'success', post }, {status: 200});
+  } catch (err) {
+    return NextResponse.json({ massage: 'Error', err }, {status: 500});
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// ブログ記事削除用API
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+
+    await main();
+    const post = await prisma.post.delete({
+      where: { id },
+    });
+    return NextResponse.json({ message: 'success', post }, {status: 200});
+  } catch (err) {
+    return NextResponse.json({ massage: 'Error', err }, {status: 500});
+  } finally {
+    await prisma.$disconnect();
+  }
+};
